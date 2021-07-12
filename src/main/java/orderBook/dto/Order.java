03145da0eef81c563293d8afbb7f0c72c8e7feb3
Cary <cwu98@users.noreply.github.com>
2021-07-12 14:48:37 -0400

    cary's commit

---
+++ src/main/java/orderBook/dto/Order.java
@@ -0,0 +1,69 @@
+package orderBook.dto;
+
+import java.math.BigDecimal;
+
+public class Order {
+    private int orderId;
+    private int clientId;
+    private String stockSymbol;
+    private String orderType;
+    private String orderStatus;
+    private int cumulativeQuantity;
+    private BigDecimal price;
+
+    public int getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(int orderId) {
+        this.orderId = orderId;
+    }
+
+    public int getClientId() {
+        return clientId;
+    }
+
+    public void setClientId(int clientId) {
+        this.clientId = clientId;
+    }
+
+    public String getStockSymbol() {
+        return stockSymbol;
+    }
+
+    public void setStockSymbol(String stockSymbol) {
+        this.stockSymbol = stockSymbol;
+    }
+
+    public String getOrderType() {
+        return orderType;
+    }
+
+    public void setOrderType(String orderType) {
+        this.orderType = orderType;
+    }
+
+    public String getOrderStatus() {
+        return orderStatus;
+    }
+
+    public void setOrderStatus(String orderStatus) {
+        this.orderStatus = orderStatus;
+    }
+
+    public int getCumulativeQuantity() {
+        return cumulativeQuantity;
+    }
+
+    public void setCumulativeQuantity(int quantity) {
+        this.cumulativeQuantity = quantity;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+}
