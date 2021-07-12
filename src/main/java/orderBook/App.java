ea4f5f844f866883dd72f2afe0003a83cd4e814f
Cary <cwu98@users.noreply.github.com>
2021-07-12 15:24:56 -0400

    cleaned some stuff

--- src/main/java/orderBook/App.java
+++ src/main/java/orderBook/App.java
@@ -9,24 +9,21 @@ import orderBook.dto.Client;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 
-public class App implements CommandLineRunner {
+
+@SpringBootApplication
+public class App {
     @Autowired
     private JdbcTemplate jdbc;
     private static Scanner sc;
 
-
-
     public static void main(String args[]) {
         System.out.println("hello");
         SpringApplication.run(App.class, args);
-    }
-
-    @Override
-    public void run(String... args) throws Exception {
-        System.out.println("hello");
     }
 
     private void displayClientList() throws SQLException {
@@ -40,17 +37,6 @@ public class App implements CommandLineRunner {
         System.out.println("");
     }
 
-    private void addItem() throws SQLException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    private void updateItem() throws SQLException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    private void removeItem() throws SQLException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
     private static final class ClientMapper implements RowMapper<Client> {
 
         @Override
