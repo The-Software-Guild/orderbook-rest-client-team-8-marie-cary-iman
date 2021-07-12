d4b98694526801ab6339c3283209ca6fda551813
Cary <cwu98@users.noreply.github.com>
2021-07-12 15:09:16 -0400

    cary's commit

--- src/main/java/orderBook/dao/OrderBookDao.java
+++ src/main/java/orderBook/dao/OrderBookDao.java
@@ -1,4 +1,17 @@
 package orderBook.dao;
 
+import orderBook.dto.Order;
+
+import java.util.List;
+
 public interface OrderBookDao {
+    List<Order> getAllOrders();
+
+    Order getOrder(int orderId);
+
+    Order addOrder(Order newOrder);
+
+    void updateOrder(Order order);
+
+    void cancelOrder(int orderId);
 }
