47eb3d2f538753f2153b154db2071368e3b230cd
Taz <hassanein.iman@gmail.com>
2021-07-13 14:23:23 -0400

    Test passes for cancelling an order and getting current orders

--- orderbookProject/src/test/java/OrderBookDaoDBTest.java
+++ orderbookProject/src/test/java/OrderBookDaoDBTest.java
@@ -10,10 +10,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import javax.validation.constraints.AssertTrue;
 import java.math.BigDecimal;
 import java.util.List;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = TestApplicationConfiguration.class)
@@ -44,5 +45,76 @@ public class OrderBookDaoDBTest {
 
     assertEquals(order, fromDao);
   }
+
+  @Test
+  public void testGetAllOrders() {
+    // Add two new orders to the database
+    Order firstOrder = new Order();
+    firstOrder.setClientId(1);
+    firstOrder.setOrderType("BID");
+    firstOrder.setOrderStatus("New");
+    firstOrder.setStockSymbol("TSLA");
+    firstOrder.setCumulativeQuantity(50);
+    firstOrder.setPrice(new BigDecimal("20.00"));
+    firstOrder = orderDao.addOrder(firstOrder);
+
+    Order secondOrder = new Order();
+    secondOrder.setClientId(1);
+    secondOrder.setOrderType("ASK");
+    secondOrder.setOrderStatus("Canceled");
+    secondOrder.setStockSymbol("TSLA");
+    secondOrder.setCumulativeQuantity(35);
+    secondOrder.setPrice(new BigDecimal("35.25"));
+    secondOrder = orderDao.addOrder(secondOrder);
+
+    Order firstFromDao = orderDao.getOrder(firstOrder.getOrderId());
+    Order secondFromDao = orderDao.getOrder(secondOrder.getOrderId());
+
+    List<Order> allOrders = orderDao.getAllOrders();
+
+    assertEquals(2, allOrders.size());
+    assertTrue(allOrders.contains(firstFromDao));
+    assertTrue(allOrders.contains(secondFromDao));
+  }
+
+  @Test
+  public void testCancelAndGetCurrentOrders() {
+    // Add two new orders to the database
+    Order firstOrder = new Order();
+    firstOrder.setClientId(1);
+    firstOrder.setOrderType("BID");
+    firstOrder.setOrderStatus("New");
+    firstOrder.setStockSymbol("TSLA");
+    firstOrder.setCumulativeQuantity(50);
+    firstOrder.setPrice(new BigDecimal("20.00"));
+    firstOrder = orderDao.addOrder(firstOrder);
+
+    Order secondOrder = new Order();
+    secondOrder.setClientId(1);
+    secondOrder.setOrderType("ASK");
+    secondOrder.setOrderStatus("New");
+    secondOrder.setStockSymbol("TSLA");
+    secondOrder.setCumulativeQuantity(35);
+    secondOrder.setPrice(new BigDecimal("35.25"));
+    secondOrder = orderDao.addOrder(secondOrder);
+
+    Order firstFromDao = orderDao.getOrder(firstOrder.getOrderId());
+    Order secondFromDao = orderDao.getOrder(secondOrder.getOrderId());
+
+    List<Order> currentOrders = orderDao.getCurrentOrders();
+
+    assertEquals(2, currentOrders.size());
+    assertTrue(currentOrders.contains(firstFromDao));
+    assertTrue(currentOrders.contains(secondFromDao));
+
+    orderDao.cancelOrder(secondFromDao.getOrderId());
+    currentOrders = orderDao.getCurrentOrders();
+
+    assertEquals(1, currentOrders.size());
+    assertTrue(currentOrders.contains(firstFromDao));
+    assertFalse(currentOrders.contains(secondFromDao));
+  }
+
+
 
 }
