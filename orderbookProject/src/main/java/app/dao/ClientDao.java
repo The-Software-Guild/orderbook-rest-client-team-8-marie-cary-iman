e9565108b284d04f992e13f513ae9fe6cf6aa4ee
Taz <hassanein.iman@gmail.com>
2021-07-14 12:56:06 -0400

    ClientDao fully tested, OrderDao fully tested, starting tradeDao

--- orderbookProject/src/main/java/app/dao/ClientDao.java
+++ orderbookProject/src/main/java/app/dao/ClientDao.java
@@ -7,6 +7,8 @@ import java.util.List;
 public interface ClientDao {
     List<Client> getAllClients();
 
+    Client getClientById(int clientId);
+
     boolean deleteClientById(int clientId);
 
     Client addClient(Client newClient);
